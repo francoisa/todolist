ef12bbdaeb392906f027d755827615a8efb3a210
francoisa <andre.francois@gmail.com>
2016-12-02 03:04:48 -0500

    All packages configured.

---
+++ post-create-app.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+npm install --save redux
+npm install --save react-redux
+npm install --save-dev redux-devtools
+npm install react-bootstrap --save
+npm install bootstrap@3 --save
+endlocal
